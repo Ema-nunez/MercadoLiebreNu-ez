b1514e0b7af85aba1b81385e88d6625878c3818d
jhonatannunez <jhonatanchipana4@gmail.com>
2021-11-23 11:05:24 -0300

    añadiendo lo necesario para heroku

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando estilos');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando correctamente');
 });
 
 app.get('/home', (req,res)=>{
